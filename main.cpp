0cb134a487230ff36d4a1b5b34cd3a7c1f7443d0
nicwenzel26 <nwenzel@mymail.mines.edu>
2023-10-03 17:10:29 -0600

    Added prompt for character name.

--- main.cpp
+++ main.cpp
@@ -16,8 +16,15 @@ int main()
         std::cin >> response;
     }
 
+    // Handling when a user says 'no' when asked if they want to make a character
     if (response == 'n') {
         std::cout << "OK! See you later!\n";
         exit(0);
     }
+
+    // Player does want to make a character
+    std::string tempCharName;
+    std::cout << "Great! First we need to figure out what your character is called.\n";
+    std::cout << "What is your character's name? ";
+    std::cin >> tempCharName;
 }
