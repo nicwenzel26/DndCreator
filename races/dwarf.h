9ddaf03db5f23db2cbf7b217c05e9218bd39674a
nicwenzel26 <nwenzel@mymail.mines.edu>
2023-10-06 14:52:58 -0600

    Added shell function to adjust traits for dwarf character

---
+++ races/dwarf.h
@@ -0,0 +1,14 @@
+#ifndef DWARF_H
+#define DWARF_H
+
+#include "../character.hpp"
+
+////////////////////////////////////////////////////////////////////////
+// @brief Function to adjust the traits of the passed in character for
+//        a dwarf
+// @param Character character
+//////////////////////////////////////////////////////////////////////// 
+void setDwarfTraits(Character character);
+
+
+#endif // DWARF_H
