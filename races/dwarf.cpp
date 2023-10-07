abf21a3b9910c769e709fd37cbdb562ca9c80c05
nicwenzel26 <nwenzel@mymail.mines.edu>
2023-10-07 08:46:04 -0600

    Added some functionality to setDwarfTraits

--- races/dwarf.cpp
+++ races/dwarf.cpp
@@ -1,11 +1,13 @@
 #include "dwarf.h"
+#include "../util.h"
 
 ////////////////////////////////////////////////////////////////////////
 // @brief Function to adjust the traits of the passed in character for
 //        a dwarf
 // @param Character character
 //////////////////////////////////////////////////////////////////////// 
-void setDwarfTraits(Character character)
+void setDwarfTraits(Character &character)
 {
-
+    character.setSize(dwarfSize);
+    character.updateCon(dwarfConBonus);
 }
--- races/dwarf.h
+++ races/dwarf.h
@@ -3,12 +3,15 @@
 
 #include "../character.hpp"
 
+int dwarfConBonus = 2;
+Sizes dwarfSize   = Sizes::Medium;
+
 ////////////////////////////////////////////////////////////////////////
 // @brief Function to adjust the traits of the passed in character for
 //        a dwarf
 // @param Character character
 //////////////////////////////////////////////////////////////////////// 
-void setDwarfTraits(Character character);
+void setDwarfTraits(Character &character);
 
 
 #endif // DWARF_H
